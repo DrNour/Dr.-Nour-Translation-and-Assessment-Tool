7b343228135f56466cf6eca22e0a8dc2ef8793af
Dr. Noureldin Abdelaal <43105891+DrNour@users.noreply.github.com>
2025-09-27 18:00:50 +0400

    Update student_interface.py

--- student_interface.py
+++ student_interface.py
@@ -1,21 +1,8 @@
-# student_interface.py
 import streamlit as st
 
-def run_feature():
+def student_panel():
     st.header("Student Interface")
-    
-    st.subheader("Submit Translation")
-    translation_text = st.text_area("Your Translation")
-    reference_text = st.text_area("Optional Reference Text")
-    
-    translate_type = st.radio("Choose Translation Mode", ["Translate from Scratch", "Post-Edit Translation"])
-    
-    if st.button("Submit Translation"):
-        if not translation_text:
-            st.warning("Translation text cannot be empty.")
-        else:
-            # TODO: Save student submission
-            st.success("Submission received! (placeholder)")
-    
-    st.subheader("Gamification & Leaderboard")
-    st.info("Points, badges, and leaderboard will be displayed here (placeholder).")
+    translation = st.text_area("Submit your translation:")
+    post_editing = st.checkbox("Post-editing mode")
+    if st.button("Submit"):
+        st.success("Translation submitted!")
