8465ebe5a8eef845616606b7086218d1bda26636
Dr. Noureldin Abdelaal <43105891+DrNour@users.noreply.github.com>
2025-09-28 15:49:42 +0400

    Update instructor.py

--- modules/instructor.py
+++ modules/instructor.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
-
 def instructor_dashboard():
     st.header("Instructor Dashboard")
     st.info("Here you can create exercises, view submissions, etc.")
-
 def instructor_interface(exercises, submissions):
     st.header("Instructor Dashboard")
 
@@ -69,4 +67,5 @@ def instructor_interface(exercises, submissions):
                 file_name="submissions.csv",
                 mime="text/csv",
             )
+
 
