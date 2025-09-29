def32b314aba52fab2ff8eae713808881826e4c6
Dr. Noureldin Abdelaal <43105891+DrNour@users.noreply.github.com>
2025-09-29 20:03:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-import streamlit as st
-import importlib.util
+import sys
 import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "modules"))
 
-# Function to import a module from file
-def import_module_from_path(name, path):
-    spec = importlib.util.spec_from_file_location(name, path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
-
-BASE_DIR = os.path.dirname(__file__)
-STUDENT_MODULE = import_module_from_path("student_interface", os.path.join(BASE_DIR, "modules", "student_interface.py"))
-INSTRUCTOR_MODULE = import_module_from_path("instructor_interface", os.path.join(BASE_DIR, "modules", "instructor_interface.py"))
+import streamlit as st
+import student_interface
+import instructor_interface
 
 st.sidebar.title("Navigation")
 choice = st.sidebar.radio("Go to", ["Student", "Instructor"])
 
 if choice == "Student":
-    STUDENT_MODULE.student_dashboard()
+    student_interface.student_dashboard()
 else:
-    INSTRUCTOR_MODULE.instructor_dashboard()
+    instructor_interface.instructor_dashboard()
