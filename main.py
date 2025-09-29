9030c8ba23a5d3f4cb8f0db8deb741e890c30bfc
Dr. Noureldin Abdelaal <43105891+DrNour@users.noreply.github.com>
2025-09-29 19:10:21 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import streamlit as st
 from modules import student_interface, instructor_interface
 
-def main():
-    st.sidebar.title("Navigation")
-    choice = st.sidebar.radio("Go to", ["Student", "Instructor"])
+st.sidebar.title("Navigation")
+choice = st.sidebar.radio("Go to", ["Student", "Instructor"])
 
-    if choice == "Student":
-        student_interface.student_dashboard()
-    elif choice == "Instructor":
-        instructor_interface.instructor_dashboard()
-
-if __name__ == "__main__":
-    main()
+if choice == "Student":
+    student_interface.student_dashboard()
+else:
+    instructor_interface.instructor_dashboard()
