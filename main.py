d2305c83b55cfc025e171e3c3323a05d1d8f6cc6
Dr. Noureldin Abdelaal <43105891+DrNour@users.noreply.github.com>
2025-09-29 19:43:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import sys
-import os
-
-# Add modules folder to Python path (fail-proof)
-sys.path.append(os.path.join(os.path.dirname(__file__), "modules"))
-
 import streamlit as st
-import student_interface
+from modules import student_interface, instructor_interface
 
 st.sidebar.title("Navigation")
 choice = st.sidebar.radio("Go to", ["Student", "Instructor"])
 
 if choice == "Student":
     student_interface.student_dashboard()
+else:
+    instructor_interface.instructor_dashboard()
